e29926a5fd65bfce920014c30373c6d5236b3ad7
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-09-10 17:23:25 -0300

    Preparcion galeria de productos

--- trajes.php
+++ trajes.php
@@ -29,14 +29,45 @@
 			<section class="Columnas">
 				<div class="Columnas-1">
 					<img src="public/images/logo.png" alt="">
+					<ul>
+						<li><a href="">TRAJES</a></li>
+						<li><a href="">CAMISAS</a></li>
+						<li><a href="">CORBATAS</a></li>
+						<li><a href="">PANTALONES</a></li>
+						<li><a href="">ZAPATOS</a></li>
+						<li><a href="">CINTURONES</a></li>
+						<li><a href="">ACCESORIOS</a></li>
+					</ul>
 				</div> <!-- / Columnas-1 -->
 
 				<div class="Columnas-2">
-					Primer fila de ropa o indumentarios
+					<p>
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptates harum alias molestias hic enim, mollitia dolorum! Velit minus officiis, quisquam pariatur sint architecto nam ipsam dignissimos provident consectetur eius!</span>
+						<span>Blanditiis qui amet in iure magnam, accusantium cumque veniam ipsam consectetur quas tempora fuga, repellat ducimus, inventore, magni expedita non beatae architecto sunt nesciunt temporibus. Eos, laudantium sit inventore neque.</span>
+						<span>Ratione doloribus aspernatur consequuntur itaque aperiam magni incidunt qui, quaerat deserunt quisquam voluptatibus ipsam repudiandae, reprehenderit deleniti impedit omnis temporibus corporis facilis, suscipit beatae consequatur fugiat perferendis nam. Autem, suscipit!</span>
+						<span>Similique totam, accusamus fuga et adipisci at cum est sunt. Harum nobis, exercitationem, id dolorem maiores iure itaque, dolores aut, aliquid totam animi cupiditate? Sapiente, voluptates est sequi ducimus nemo!</span>
+						<span>In, ipsa! Minus adipisci sapiente sequi laborum nostrum, nam, repudiandae aliquam autem nesciunt vitae tempora corporis animi mollitia possimus iusto architecto, provident eveniet. Ducimus quia, ipsam, accusamus velit quod officia.</span>
+						<span>Asperiores ipsam, enim commodi assumenda quae a illum reprehenderit quis ab nihil aperiam atque aspernatur, reiciendis fuga soluta magnam tempore mollitia libero deleniti praesentium placeat ipsa possimus! Hic, magnam, repudiandae.</span>
+						<span>Neque modi accusantium repudiandae voluptatem nam deserunt non magnam architecto earum dolor soluta natus odit provident nostrum, suscipit cupiditate est tenetur, at sint fugiat impedit, ipsam. Recusandae tenetur asperiores optio.</span>
+						<span>Soluta, consectetur molestias autem fugit expedita dicta perferendis, neque esse ipsam repellat, aliquid, distinctio illum fuga pariatur. Ab accusamus magnam, tempore enim et atque, dicta maxime eaque nostrum nihil veniam!</span>
+						<span>Consectetur facere nemo, incidunt minus eveniet aliquid aspernatur deserunt maxime dicta repellendus tempore magni, iste pariatur, saepe dolorem voluptate a veniam, porro laboriosam et impedit! A iste dolores deleniti facilis.</span>
+						<span>Distinctio, nam totam quis, facere molestias non, nesciunt aliquid, corporis doloremque in cumque aperiam quam rerum odit. Labore, doloremque, quasi. Natus tenetur ullam voluptates magnam dolorem facilis doloribus distinctio quis!</span>
+					</p>
 				</div> <!-- / Columnas-2 -->
 
 				<div class="Columnas-3">
-					Segunda fila de ropa o indumentarios
+					<p>
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia velit beatae rem deleniti non impedit nulla autem neque veniam ex assumenda temporibus repudiandae unde, molestias, reiciendis distinctio quibusdam, maiores consectetur.</span>
+						<span>Unde eligendi, voluptas quibusdam nostrum vel praesentium quo fugiat beatae atque doloribus eos illum iste maxime laudantium minus blanditiis voluptatum. Nesciunt iste sit esse sed, earum aspernatur, debitis veniam voluptatibus.</span>
+						<span>Ut ducimus explicabo deleniti ea impedit ipsa quod placeat nesciunt! Exercitationem aut provident, ipsa saepe, similique nesciunt aliquam facere quis impedit! Minima reiciendis minus quo, officiis nulla voluptate molestiae illo.</span>
+						<span>Consectetur et exercitationem mollitia commodi error voluptatum facilis amet voluptatem laudantium. Incidunt ut laudantium illo sequi. Ratione deleniti quibusdam, culpa vitae qui mollitia consectetur expedita temporibus facilis, voluptatibus veniam iure.</span>
+						<span>Repudiandae, laboriosam. Nesciunt facilis vitae magni, molestiae fugit, officia unde impedit soluta. Optio commodi aliquam adipisci dignissimos animi, expedita necessitatibus culpa temporibus delectus magnam, tenetur voluptatibus rem, velit praesentium enim!</span>
+						<span>Eius omnis hic in similique, consequuntur at ea, error accusamus suscipit, magni, aut sed quaerat dolores excepturi. Maiores corrupti sunt, amet quasi voluptatibus fuga, mollitia, provident nam veritatis officiis possimus.</span>
+						<span>Officia pariatur quaerat, temporibus laborum dolore officiis explicabo qui fugiat praesentium distinctio, a unde deleniti, aperiam ex vitae repellendus doloremque dolorum minima, modi nobis inventore facere quia? Excepturi, modi, sequi.</span>
+						<span>Asperiores enim veritatis odio nihil ipsum nemo, nobis, iure omnis suscipit! Fuga sint modi sapiente eveniet reprehenderit quibusdam incidunt, reiciendis repellendus numquam, aliquam labore iste quidem mollitia illo veritatis eos.</span>
+						<span>Sit obcaecati nesciunt nemo dolor officiis, inventore iste cupiditate asperiores praesentium iure perferendis molestiae, minima minus eligendi quia ex in. Unde libero aliquid tenetur, autem quas. Quas aliquid, consectetur enim.</span>
+						<span>Asperiores deleniti, perspiciatis nostrum molestias, saepe iure nam modi fuga doloremque dolorem consequuntur laudantium aliquam dolor ipsum sequi mollitia harum voluptatem itaque. Molestias culpa, error quo veritatis aperiam minus eos.</span>
+					</p>
 				</div> <!-- / Columnas-3 -->
 			</section> <!-- / Columnas -->
 
