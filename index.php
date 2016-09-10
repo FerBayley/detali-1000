b86737114803f34c6315a933578f198f8d298e0b
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-09-10 16:49:01 -0300

    Finalizacion del home page

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+<head>
+	<meta charset="UTF-8">
+	<title>Detali Sastreria Italiana</title>
+	<meta name="description" content="Sastreria de hombres estilo Italia ubicada en centro porteño con años de trayectoria vistiendo al hombre moderno">
+
+	<!-- VIEWPORT -->
+	<meta name="viewport" content="width=device-width, user-scalable=no"/>
+	<!-- VIEWPORT -->
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="public/css/normalize.css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="public/css/main.css" />
+	<!-- CSS -->
+
+	<!-- FAVICON -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<!-- FAVICON -->
+
+</head>
+<body>
+
+	<section class="Contenedor">
+		
+		<header>
+			<section class="Tarjeta">
+				<p class="Tarjeta-texto">
+					<a href="http://www.detali.com.ar/landing/tarjeta/">
+						Obtené un 20% 30% y 40% OFF con tu tarjeta Detalí - Pedila acá
+					</a>
+				</p> <!-- / Tarjeta-texto -->
+			</section> <!-- / Tarjeta -->
+		</header> <!-- / header -->
+
+		<section class="Menu">
+			<ul>
+				<li><a href="#">INICIO</a></li> |
+				<li><a href="#">A MEDIDA</a></li> |
+				<li><a href="#">QUIENES SOMOS?</a></li> |
+				<li><a href="#">CONDICIONES GENERALES</a></li> |
+				<li><a href="#">SHOWROOM</a></li> |
+				<li><a href="#">SASTRERIA A DOMICILIO</a></li>
+			</ul>
+		</section> <!-- / Menu -->
+
+		<nav>
+			<img src="public/images/logo.png" class="Logo" alt="Detali Sastreria Italiana">
+
+			<ul>
+				<li><a href="#">TRAJES</a></li>
+				<li><a href="#">CAMISAS</a></li>
+				<li><a href="#">CORBATAS</a></li>
+				<li><a href="#">PANTALONES</a></li>
+				<li><a href="#">ZAPATOS</a></li>
+				<li><a href="#">CINTURONES</a></li>
+				<li><a href="#">ACCESORIOS</a></li>
+			</ul>
+		</nav> <!-- / nav -->
+
+		<footer>
+			<div class="Redes">
+				<ul>
+					<li>&copy; 2015 | Av Roque Saenz Peña 615 | Piso 5 Oficinas 512 - Tel: 4-328-9656 / <a href="mailto:sastreria@detali.com.ar">sastreria@detali.com.ar</a></li>
+					<li><a href="https://www.facebook.com/SastreriaDetali" target="_blank">
+						<img src="public/images/facebook.png" alt="Facebook" width="20px" height="20px">
+					</a></li>
+					<li><a href="https://www.facebook.com/SastreriaDetali" target="_blank">
+						<img src="public/images/twitter.png" alt="Twitter" width="20px" height="20px">
+					</a></li>
+					<li><a href="https://www.pinterest.com/detali" target="_blank">
+						<img src="public/images/pinterest.png" alt="Pinterest" width="20px" height="20px">
+					</a></li>
+					<li><a href="https://instagram.com/detalisastreria/" target="_blank">
+						<img src="public/images/instagram.png" alt="Instagram" width="20px" height="20px">
+					</a></li>
+					<li><a href="https://ar.linkedin.com/in/detali" target="_blank">
+						<img src="public/images/linkedin.png" alt="Linkedin" width="20px" height="20px">
+					</a></li>
+				</ul>
+			</div> <!-- / Redes -->
+		</footer> <!-- / footer -->
+
+	</section> <!-- / Contenedor -->
+	
+</body>
+</html>
