0d0b11b22f76ac96fe22527b484bc5367307048c
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-09-13 01:00:11 -0300

    Links menu

--- public/includes/header.php
+++ public/includes/header.php
@@ -10,11 +10,11 @@
 <span class="Burguer" id="Burguer-button">MENU</span>
 <section class="Menu" id="menu">
 	<ul>
-		<li><a href="#">INICIO</a></li> <span class="Pleca">|</span>
-		<li><a href="#">A MEDIDA</a></li> <span class="Pleca">|</span>
-		<li><a href="#">QUIENES SOMOS?</a></li> <span class="Pleca">|</span>
-		<li><a href="#">CONDICIONES GENERALES</a></li> <span class="Pleca">|</span>
-		<li><a href="#">SHOWROOM</a></li> <span class="Pleca">|</span>
-		<li><a href="#">SASTRERIA A DOMICILIO</a></li>
+		<li><a href="http://www.detali.com.ar">INICIO</a></li> <span class="Pleca">|</span>
+		<li><a href="http://detali.com.ar/a-medida.php">A MEDIDA</a></li> <span class="Pleca">|</span>
+		<li><a href="http://detali.com.ar/quienes-somos.php">QUIENES SOMOS?</a></li> <span class="Pleca">|</span>
+		<li><a href="http://detali.com.ar/condiciones-generales.php">CONDICIONES GENERALES</a></li> <span class="Pleca">|</span>
+		<li><a href="http://detali.com.ar/showroom.php">SHOWROOM</a></li> <span class="Pleca">|</span>
+		<li><a href="http://detali.com.ar/sastreria-domicilio.php">SASTRERIA A DOMICILIO</a></li>
 	</ul>
 </section> <!-- / Menu -->
